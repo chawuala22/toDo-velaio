7497ad14642488e7590d569ba841938896b3b874
MiguelR <mrojasgarcia22@gmail.com>
2024-09-28 18:49:31 -0500

    Update method added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,25 +1,24 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {  DataTask, RequestTask, Task } from '../interfaces/task';
+import { DataTask, RequestTask, Task } from '../interfaces/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-urlBase: string = 'https://tasks-pearl.vercel.app/task'
-  constructor(private _httpClient : HttpClient) { }
-
+  urlBase: string = 'https://tasks-pearl.vercel.app/task';
+  
+  constructor(private _httpClient: HttpClient) {}
 
   getTasks() {
     return this._httpClient.get<RequestTask>(this.urlBase);
   }
 
-  createTask(task : Task){
-    return this._httpClient.post<Task>('http://localhost:3000/tasks', task);
+  createTask(task: Task) {
+    return this._httpClient.post<Task>(this.urlBase, task);
   }
 
-  // updateTask(task : Task){
-  //   return this._httpClient.put(`http://localhost:3000/tasks/${task._id}`, task);
-  // }
-
+  updateTask(id: string, task: Task) {
+    return this._httpClient.patch<Task>(`${this.urlBase}/${id}`, task);
+  }
 }
