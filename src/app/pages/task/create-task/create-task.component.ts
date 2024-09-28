bac3616db2296f7a7ff159b8e0292f840ab9fb7a
MiguelR <mrojasgarcia22@gmail.com>
2024-09-28 17:26:26 -0500

    header is added

--- src/app/pages/task/create-task/create-task.component.ts
+++ src/app/pages/task/create-task/create-task.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-create-task',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './create-task.component.html',
   styleUrl: './create-task.component.scss'
 })
--- src/app/pages/task/task.routes.ts
+++ src/app/pages/task/task.routes.ts
@@ -3,21 +3,29 @@ import { Routes } from '@angular/router';
 export const taskRoutes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./view-task/view-task.component').then(m => m.ViewTaskComponent),
-    children: [
-      {
-        path: 'create-task',
-        loadComponent: () => import('./create-task/create-task.component').then(m => m.CreateTaskComponent)
-      },
-      {
-        path: 'view-detail/:id',
-        loadComponent: () => import('./detail-task/detail-task.component').then(m => m.DetailTaskComponent)
-      },
-      {
-        path: '', redirectTo: 'home', pathMatch: 'full'
-      }
-    ]
+    loadComponent: () =>
+      import('./view-task/view-task.component').then(
+        (m) => m.ViewTaskComponent
+      ),
   },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  {
+    path: 'create-task',
+    loadComponent: () =>
+      import('./create-task/create-task.component').then(
+        (m) => m.CreateTaskComponent
+      ),
+  },
+  {
+    path: 'view-detail/:id',
+    loadComponent: () =>
+      import('./detail-task/detail-task.component').then(
+        (m) => m.DetailTaskComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
-
