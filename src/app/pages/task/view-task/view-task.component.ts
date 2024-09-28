afff5d72396a300f78fa4dcd37f65badb18043f3
MiguelR <mrojasgarcia22@gmail.com>
2024-09-28 18:40:57 -0500

    Task list view added

--- src/app/pages/task/view-task/view-task.component.ts
+++ src/app/pages/task/view-task/view-task.component.ts
@@ -3,30 +3,36 @@ import { TaskService } from '../../../services/task.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Task } from 'zone.js/lib/zone-impl';
 import { DataTask } from '../../../interfaces/task';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-task',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './view-task.component.html',
   styleUrl: './view-task.component.scss',
 })
 export class ViewTaskComponent implements OnInit {
-  array_task : DataTask[] = [];
-  constructor(private _serviceTask: TaskService) {}
+  array_task: DataTask[] = [];
+  constructor(private _serviceTask: TaskService, private router: Router) {}
   ngOnInit(): void {
     this.getTask();
   }
 
-  getTask(){
+  getTask() {
     this._serviceTask.getTasks().subscribe({
       next: (tasks) => {
-        this.array_task = tasks.data
+        this.array_task = tasks.data;
         console.log(this.array_task);
       },
       error: (error) => {
         console.error(error);
       },
-    })
+    });
+  }
+
+  goToDetail(taskId: string) {
+    this.router.navigate([`/view-detail/${taskId}`]);
   }
 }
