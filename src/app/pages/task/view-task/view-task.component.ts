3222b9e541cf4ad5da2dcf410e1635864ab886a8
MiguelR <mrojasgarcia22@gmail.com>
2024-09-28 21:13:05 -0500

    Filter is added

--- src/app/pages/task/view-task/view-task.component.ts
+++ src/app/pages/task/view-task/view-task.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../../services/task.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Task } from 'zone.js/lib/zone-impl';
 import { DataTask } from '../../../interfaces/task';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -11,11 +10,13 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [HttpClientModule, CommonModule],
   templateUrl: './view-task.component.html',
-  styleUrl: './view-task.component.scss',
+  styleUrls: ['./view-task.component.scss'],
 })
 export class ViewTaskComponent implements OnInit {
-  array_task: DataTask[] = [];
+  array_task: DataTask[] = []; // Esta es la lista filtrada que se mostrará
+  original_task: DataTask[] = []; // Mantiene los datos originales
   constructor(private _serviceTask: TaskService, private router: Router) {}
+
   ngOnInit(): void {
     this.getTask();
   }
@@ -23,7 +24,9 @@ export class ViewTaskComponent implements OnInit {
   getTask() {
     this._serviceTask.getTasks().subscribe({
       next: (tasks) => {
-        this.array_task = tasks.data;
+        this.original_task = tasks.data; // Asigna los datos originales
+        this.array_task = [...this.original_task]; // Copia para mostrar y filtrar
+        console.log(this.array_task);
       },
       error: (error) => {
         console.error(error);
@@ -33,5 +36,21 @@ export class ViewTaskComponent implements OnInit {
 
   goToDetail(taskId: string) {
     this.router.navigate([`/view-detail/${taskId}`]);
+  }
+
+  onSelectChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    console.log(selectElement.value);
+    this.filterTasks(selectElement.value);
+  }
+
+  filterTasks(selectedValue: string) {
+    if (selectedValue === '1') {
+      this.array_task = this.original_task.filter((task) => task.state === false);
+    } else if (selectedValue === '2') {
+      this.array_task = this.original_task.filter((task) => task.state === true);
+    } else {
+      this.array_task = [...this.original_task];
+    }
   }
 }
